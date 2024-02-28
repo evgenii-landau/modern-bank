6c3079dcfd072e573de6a4cbf9d54f99c8efaa07
Evgenii Kotenev <koteniov.zgenya@mail.ru>
2024-02-28 09:51:44 +0300

    business section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -33,9 +33,9 @@ export const Hero = () => {
 					className='font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100px] leading-[75px] w-full'>Payment
 					Method
 				</h1>
-				<p className={`${styles.paragraph} max-w-[470px] mt-5`}>With the right credit card, you can improve your
-					financial life by building credit, earning rewards and
-					saving money. But with hundreds of credit cards on the market.</p>
+				<p className={`${styles.paragraph} max-w-[470px] mt-5`}>Our team of experts uses a methodology to identify the
+					credit cards most likely to fit your needs.
+					We examine annual percentage rates, annual fees.</p>
 			</div>
 
 			<div className={`flex-1 flex ${styles.flexCenter} md:my-0-10 relative`}>
